2d6070e462ddd717657ca7563ee4a30102437adb
Ezzamel <ezzamel@randstad.com.my>
2020-04-13 09:23:12 +0800

    Implemented carousel and lodash example

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <slot></slot>    
+    </div> 
+</template>
+
+<script>
+    import 'flickity/dist/flickity.min.css'
+    import Flickity from 'flickity'
+    export default {
+        props : {
+            wrapAround: { default: true },
+            autoPlay: { default:false }
+        },
+        mounted() {
+            new Flickity(this.$el, {
+                wrapAround: this.wrapAround,
+                contain: true,
+                cellAlign: "left",
+                autoPlay: this.autoPlay
+            })
+        }
+    }
+</script>
---
+++ resources/js/components/LodashExample.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button @click="alertDebounce($event.target.innerHTML)">Debounce</button>
+        <button @click="alertThrottle($event.target.innerHTML)">Throttle</button>
+    </div>
+</template>
+
+<script>
+import _ from 'lodash';
+export default {
+    methods: {
+        alertDebounce: _.debounce(function(msg) {
+            console.log(msg);
+        }, 2000),
+        alertThrottle: _.throttle(function(msg) {
+            console.log(msg);
+        }, 2000)
+    }
+}
+</script>
